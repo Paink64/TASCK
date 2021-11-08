b4c73f52431489ce2b43dccf7d35a03ba3c2070e
Matthew De La Cruz <mdc0195@gmail.com>
2021-11-08 03:42:07 -0800

    VSCode stuff is wierd. Added the missing files

--- src/MyTasks.js
+++ src/MyTasks.js
@@ -3,19 +3,17 @@ import { Link } from 'react-router-dom' ;
 import React from 'react'
 import { useState } from "react";
 import users from './App';
+import Tasks from './components/Tasks.js';
+
 
 const MyTasks = ({user}) => {
-    return (
-        <>
-          {user.isOnProject ? <h1 style={{textAlign : 'center'}}>Hello, here are your tasks!</h1> 
-            : <h1 style={{ textAlign : 'center',
-            marginTop: '200px'}}>Please create or add a project before visiting this page</h1>}
-          <h2 style={{textAlign:'center'}}><Link to='/'>
-              <Button text='Go Back' color='black'></Button>
-          </Link> </h2> 
-          
-        </>
-    )
+  return (
+    <div className="App">
+      <div className="container">
+        <Tasks tasks={user.tasks}></Tasks>
+      </div>
+    </div>
+  );
 }
 
 export default MyTasks
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import Task from "./Task";
 
-const Tasks = ({ tasks }) => {
-    return (
-        <div>
-            
-        </div>
-    )
+/*
+This file is has a new button at the top of the page
+It then receives tasks then breaks it down into its components eg: title, desc, etc
+then feeds broken down pieces of task into Task.js the render component using the id as a identifier in the task array.
+*/ 
+function Tasks({ tasks }) {
+  return (
+    /**
+      Presently the New button does nothing.
+      This is simply the New button. Nothing else.
+      TODO: Fix conflicting CSS
+     */
+    <div className="row">
+      <div className="col-12 text-right">
+        <button className="button primary">New</button>
+      </div>
+       {tasks.map((task) => (
+        <Task task={task} key={task.taskID} />
+      ))}
+      <div className="col-12"></div>
+    </div>
+  );
 }
 
-export default Tasks
+export default Tasks;
