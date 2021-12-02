811773fd7bf0a7dfed4c4db127dbe0150ae2f9a9
MarcoC09 <56285125+MarcoC09@users.noreply.github.com>
2021-12-01 16:47:05 -0800

    Update Profile.js

--- src/Profile.js
+++ src/Profile.js
@@ -4,20 +4,33 @@ import { useState } from "react";
 import { Link } from 'react-router-dom' ;
 import users from './App';
 import userDummy from './components/User';
+import firebase from './components/Firebase';
 
-const Profile = ({user}) => {
+const Profile = () => {
+  const [userFullName, setUserFullName] = useState();
+  const [userName, setUserName] = useState();
+  const [lastLogin, setLastLogin] = useState();
+
+  const user = firebase.auth().currentUser;
+  const userRef = firebase.database().ref('users/'+ user.uid);
+
+  userRef.get().then((snapshot) =>{
+    const data = snapshot.val();
+    setUserFullName(data.full_name);
+    setUserName(data.userName);
+    setLastLogin(data.lastLogin);
+
+  })
+
     return (
         <>
-          {userDummy.isLoggedOn ? 
           <div>
-          <h1 style={{textAlign : 'center'}}>{userDummy.name}'s profile</h1> 
-          <h2>Username: {userDummy.username}</h2>
-          <h2>Email: {userDummy.email}</h2>
-          <h2>UserId: {userDummy.userId}</h2>
-          <h2>Last Login: {userDummy.lastLogin}</h2>
+          <h1 style={{textAlign : 'center'}}>{userFullName}'s profile</h1> 
+          <h2>Username: {userName}</h2>
+          <h2>Email: {user.email}</h2>
+          <h2>UserId: {user.uid}</h2>
+          <h2>Last Login: {lastLogin}</h2>
           </div>
-            : <h1 style={{ textAlign : 'center',
-            marginTop: '200px'}}>Please create or add a project before visiting this page</h1>}
           <h2 style={{textAlign:'center'}}><Link to='/'>
               <Button text='Go Back' color='black'></Button>
           </Link> </h2> 
